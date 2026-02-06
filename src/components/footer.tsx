8fff442e3fcaa07197323cca18a7410e04f7801e
Private Account <pa2952867@gmail.com>
2026-02-06 13:56:55 +0000

    change the name to Abhiram Yadav and logo to AR.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,12 +4,12 @@ export function Footer() {
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Designed and Developed by Soumyajit Behera
+            Designed and Developed by Abhiram Yadav
           </p>
         </div>
         <div>
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Copyright © 2026 SB
+            Copyright © 2026 AY
           </p>
         </div>
       </div>
